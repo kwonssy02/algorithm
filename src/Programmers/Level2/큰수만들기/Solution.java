6cc5931d76f6c2b4569f926f997c5f7299aea1a8
권혁찬 <kwonhc@hyundai-autoever.com>
2021-04-15 23:53:13 +0900

    큰수만들기 improved

--- src/Programmers/Level2/큰수만들기/Solution.java
+++ src/Programmers/Level2/큰수만들기/Solution.java
@@ -1,7 +1,31 @@
 // https://programmers.co.kr/learn/courses/30/lessons/42883
 package Programmers.Level2.큰수만들기;
 
+import java.util.Stack;
+
 class Solution {
+
+    public static String solution(String number, int k) {
+        char[] arr = new char[number.length() - k];
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < number.length(); i++) {
+            char c = number.charAt(i);
+            while(!stack.isEmpty() && stack.peek() < c && k-- > 0) {
+                stack.pop();
+            }
+            stack.push(c);
+        }
+
+        // for 문 최대값을 stack size로 설정 시, k 개수만큼 빼지않은 length가 들어갈 수 있음.
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = stack.get(i);
+        }
+
+        return new String(arr);
+    }
+
+    /*
     public static String solution(String number, int k) {
         // 0번째부터 k까지 max찾고, 거기까지 substring
         // 1번째부터 k'까지 max찾고, 거기까지 substring
@@ -29,6 +53,7 @@ class Solution {
 
         return number;
     }
+     */
 
     public static void main(String[] args) {
         System.out.println(solution("11", 1));
