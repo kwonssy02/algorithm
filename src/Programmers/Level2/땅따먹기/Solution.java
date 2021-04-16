0937da78e856ba08c1cf59a2fe03bdabd423a4aa
권혁찬 <kwonhc@hyundai-autoever.com>
2021-04-17 00:39:49 +0900

    땅따먹기 시간초과

---
+++ src/Programmers/Level2/땅따먹기/Solution.java
@@ -0,0 +1,58 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12913
+package Programmers.Level2.땅따먹기;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Solution {
+    static int solution(int[][] land) {
+
+        Queue<C> queue = new LinkedList<>();
+        queue.add(new C(0, 0, 0));
+        queue.add(new C(0, 1, 0));
+        queue.add(new C(0, 2, 0));
+        queue.add(new C(0, 3, 0));
+
+        int answer = 0;
+        while(!queue.isEmpty()) {
+            C poll = queue.poll();
+            if(poll.row == land.length) {
+                answer = Math.max(answer, poll.sum);
+            } else {
+                for (int i = 0; i < 4; i++) {
+                    if(i == poll.col)
+                        continue;
+                    queue.add(new C(poll.row+1, i, poll.sum + land[poll.row][i]));
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    static class C {
+        int row;
+        int col;
+        int sum;
+
+        public C(int row, int col, int sum) {
+            this.row = row;
+            this.col = col;
+            this.sum = sum;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(
+                new int[][]{
+                        new int[]{1, 2, 3, 5},
+                        new int[]{5, 6, 7, 8},
+                        new int[]{4, 3, 2, 1},
+                        new int[]{4, 3, 2, 1},
+                        new int[]{4, 3, 2, 1},
+                        new int[]{4, 3, 2, 1},
+                        new int[]{4, 3, 2, 1}
+                }
+        ));
+    }
+}
