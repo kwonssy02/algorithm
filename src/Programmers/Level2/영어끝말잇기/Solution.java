9dd355ae1280737d890011771518ed56336fe505
권혁찬 <kwonhc@hyundai-autoever.com>
2021-04-19 00:08:17 +0900

    meeting log

---
+++ src/Programmers/Level2/영어끝말잇기/Solution.java
@@ -0,0 +1,13 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12981
+package Programmers.Level2.영어끝말잇기;
+
+class Solution {
+    public int[] solution(int n, String[] words) {
+        int[] answer = {};
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다. 
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+}
