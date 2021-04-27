300e3123310ca1bdbc471a8e5be1d9f4ae6b8427
권혁찬 <kwonhc@hyundai-autoever.com>
2021-04-27 17:20:27 +0900

    2xn타일링 solved

--- src/Programmers/Level3/_2xn타일링/Solution.java
+++ src/Programmers/Level3/_2xn타일링/Solution.java
@@ -2,23 +2,25 @@
 package Programmers.Level3._2xn타일링;
 
 class Solution {
+    // nCr로 풀 경우, 60000!를 int/long에 담기 어려움..
+    int[] fibonacci = new int[60001];
+
     // 1, 2만 사용해서 n을 만드는 경우의 수 구하기..
     public int solution(int n) {
 
-        int answer = 0;
-        for (int i = 0; i <= n/2; i++) {
-            int numOfOne = n - (i*2);
-            int numOfTwo = i;
-            // n C numOfTwo (n = numOfOne + numOfTwo)
-            // = n * n-1 * ... * (n-numOfTwo+1) / numOfTwo!
-            System.out.println("numOfOne = " + numOfOne);
-            System.out.println("numOfTwo = " + numOfTwo);
+        fibonacci[1] = 1;
+        fibonacci[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            fibonacci[i] = (fibonacci[i-1] + fibonacci[i-2]) % 1000000007;
         }
-        return answer;
+
+        return fibonacci[n];
     }
 
     public static void main(String[] args) {
         final Solution solution = new Solution();
-        solution.solution(4);
+        System.out.println(solution.solution(1));
+        System.out.println(solution.solution(10000));
     }
 }
