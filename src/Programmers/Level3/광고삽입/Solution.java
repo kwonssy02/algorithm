cce373a737de9700898248350e682136917cd8c9
권혁찬 <kwonhc@hyundai-autoever.com>
2021-05-15 22:29:56 +0900

    meetings updated

---
+++ src/Programmers/Level3/광고삽입/Solution.java
@@ -0,0 +1,9 @@
+// https://programmers.co.kr/learn/courses/30/lessons/72414
+package Programmers.Level3.광고삽입;
+
+class Solution {
+    public String solution(String play_time, String adv_time, String[] logs) {
+        String answer = "";
+        return answer;
+    }
+}
